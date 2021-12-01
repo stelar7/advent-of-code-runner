601776a0c230b3070fafe5439b14fa46e08f44f3
Querijn Heijmans <fork@irule.at>
2021-12-01 09:52:18 +0100

    Re-align tabs

--- 2021/1/test.sh
+++ 2021/1/test.sh
@@ -8,7 +8,7 @@ $D/../../lang/cmake_cpp_build.sh	"$D/solutions/cmake_cpp/querijn/"
 
 echo "-- Day 1 --"
 printf "%-10s %-20s %-8s %-10s %-10s \n" "language" "author" "total" "average" "status"
-$D/../../lang/node.sh						"$D/solutions/node/stelar7/*.mjs"		"$D/io/*"
+$D/../../lang/node.sh				"$D/solutions/node/stelar7/*.mjs"		"$D/io/*"
 $D/../../lang/cmake_cpp.sh			"$D/solutions/cmake_cpp/querijn/"		"$D/io/*"
 
 echo ""
