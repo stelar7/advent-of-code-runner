eaeefc47915bcc583f27327120db81c972febaef
stelar7 <dudedbz@gmail.com>
2021-12-01 07:25:36 +0100

    Fix day in runner

--- 2021/1/test.sh
+++ 2021/1/test.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 D=$(dirname $(realpath $0))
 
-echo "-- Day 0 --"
+echo "-- Day 1 --"
 printf "%-10s %-20s %-8s %-10s %-10s \n" "language" "author" "total" "average" "status"
 $D/../../lang/node.sh   "$D/solutions/node/stelar7/*.mjs"  "$D/io/*"
 
