86c255db83303b701f55fe414b614ba5fe173af2
arionminina <74980331+arionminina@users.noreply.github.com>
2021-12-02 18:11:28 +0100

    Added Day1 solution

---
+++ 2021/1/solutions/java/arionminina/Day1.java
@@ -0,0 +1,44 @@
+
+package day1;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
+
+import java.util.stream.*;
+
+public class Day1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        String source = "input.txt";
+        Path path = Paths.get(source);
+        List<String> lines = Files.readAllLines(path);
+        List<Integer> listInteger;
+        listInteger = lines.stream().map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> sumList = new ArrayList<>();
+        int counter = 0;
+        int counter2 = 0;
+        int filler = 0;
+
+        for (int l = 0; l < listInteger.size()-2; l++) {
+
+            sumList.add(listInteger.get(l) + listInteger.get(l + 1) + listInteger.get(l + 2));
+
+        }
+      //This technique also works for part 1
+        for (int x = 1; x < sumList.size(); x++) {
+            if (sumList.get(x) > sumList.get(x - 1)) {
+                counter2++;
+            } else {
+
+            }
+        }
+
+        System.out.println(counter2);
+
+    }
+
+}
