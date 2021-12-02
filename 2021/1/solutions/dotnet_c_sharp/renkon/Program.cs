cbfbf964cf3021a780e9724e6079855f5a388688
Fernando Martín Besteiro <fmbesteiro@gmail.com>
2021-12-02 17:40:57 -0300

    Fix problem 1

--- 2021/1/solutions/dotnet_c_sharp/renkon/Program.cs
+++ 2021/1/solutions/dotnet_c_sharp/renkon/Program.cs
@@ -1,9 +1,11 @@
-﻿int[] n = new int[2000];
+﻿List<int> n = new List<int>(2000);
 int c1 = 0, c2 = 0;
 int i = 0;
 string s;
 while ((s = Console.ReadLine()) != null)
 {
+    n.Add(0);
+
     for (int j = 0; j < s.Length; j++)
     {
         n[i] = n[i] * 10 + (s[j] - '0');
