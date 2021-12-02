c217648d63ee21dd1275caeeb44d029f5d54f9e1
stelar7 <dudedbz@gmail.com>
2021-12-02 10:21:21 +0100

    Fix sorting and fix typo

--- 2021/2/test.sh
+++ 2021/2/test.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 D=$(dirname $(realpath $0))
 
-echo "-- Day 1 --"
+echo "-- Day 2 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
 $D/../../lang/node.sh				"$D/solutions/node/stelar7/main.mjs"	"$D/io/*"
 $D/../../lang/cmake_cpp.sh			"$D/solutions/cmake_cpp/querijn/"		"$D/io/*"
--- lang/util/timeformat.sh
+++ lang/util/timeformat.sh
@@ -5,6 +5,6 @@ T=$1
 M=$((T/1000/60%60))
 S=$((T/1000%60))
 I=$((T%1000))
-(( $M > 0 )) && printf '%dm ' $M
-(( $S > 0 )) && printf '%ds ' $S
+(( $M > 0 )) && printf '%dm' $M
+(( $S > 0 )) && printf '%ds' $S
 printf '%dms\n' $I
