8c6208ba0704232223739b64ba337b6614e30eb5
stelar7 <dudedbz@gmail.com>
2021-12-02 22:10:38 +0100

    Always show time in ms

--- lang/util/timeformat.sh
+++ lang/util/timeformat.sh
@@ -5,6 +5,5 @@ T=$1
 M=$((T/1000/60%60))
 S=$((T/1000%60))
 I=$((T%1000))
-(( $M > 0 )) && printf '%dm' $M
-(( $S > 0 )) && printf '%ds' $S
-printf '%dms\n' $I
+TIME=$((($M * 60000) + ($S * 1000) + $I))
+printf '%dms\n' $TIME
