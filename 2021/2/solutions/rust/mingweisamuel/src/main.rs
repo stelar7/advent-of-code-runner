e2af1a25428c33420ed5effad56377afdc3b94c0
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-12-02 10:09:25 -0800

    Use fancier ops::Add/AddAssign

--- 2021/2/solutions/rust/mingweisamuel/src/main.rs
+++ 2021/2/solutions/rust/mingweisamuel/src/main.rs
@@ -1,5 +1,12 @@
 use std::io::BufRead;
-use std::str::FromStr;
+
+#[derive(Debug, Default)]
+pub struct Position {
+    pub position: usize,
+    pub depth_basic: usize,
+    pub depth_aimed: usize,
+    pub aim: usize,
+}
 
 #[derive(Debug)]
 pub enum Move {
@@ -7,7 +14,7 @@ pub enum Move {
     Down(u8),
     Up(u8),
 }
-impl FromStr for Move {
+impl std::str::FromStr for Move {
     type Err = Option<std::num::ParseIntError>;
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
@@ -19,13 +26,30 @@ impl FromStr for Move {
         }
     }
 }
-
-#[derive(Debug, Default)]
-pub struct Position {
-    pub position: usize,
-    pub depth_basic: usize,
-    pub depth_aimed: usize,
-    pub aim: usize,
+impl std::ops::AddAssign<Move> for Position {
+    fn add_assign(&mut self, rhs: Move) {
+        match rhs {
+            Move::Forward(n) => {
+                self.position += n as usize;
+                self.depth_aimed += (n as usize) * self.aim;
+            }
+            Move::Down(n) => {
+                self.depth_basic += n as usize;
+                self.aim += n as usize;
+            }
+            Move::Up(n) => {
+                self.depth_basic -= n as usize;
+                self.aim -= n as usize;
+            }
+        }
+    }
+}
+impl std::ops::Add<Move> for Position {
+    type Output = Self;
+    fn add(mut self, rhs: Move) -> Self::Output {
+        self += rhs;
+        self
+    }
 }
 
 fn main() {
@@ -37,23 +61,11 @@ fn main() {
                 .parse::<Move>()
                 .expect("Failed to parse move.")
         })
-        .fold(Position::default(), |mut pos, curr_move| {
-            match curr_move {
-                Move::Forward(n) => {
-                    pos.position += n as usize;
-                    pos.depth_aimed += (n as usize) * pos.aim;
-                },
-                Move::Down(n) => {
-                    pos.depth_basic += n as usize;
-                    pos.aim += n as usize;
-                },
-                Move::Up(n) => {
-                    pos.depth_basic -= n as usize;
-                    pos.aim -= n as usize;
-                },
-            }
-            pos
-        });
+        .fold(Position::default(), |pos, curr_move| pos + curr_move);
 
-    println!("{}\n{}", pos.position * pos.depth_basic, pos.position * pos.depth_aimed);
+    println!(
+        "{}\n{}",
+        pos.position * pos.depth_basic,
+        pos.position * pos.depth_aimed
+    );
 }
