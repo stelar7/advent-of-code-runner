7c2fce90a28d578f29a41f872554e3a372489b3c
Kurainu <theshiro01@gmail.com>
2021-12-03 12:44:57 +0100

    Day3

--- 2021/3/test.sh
+++ 2021/3/test.sh
@@ -6,6 +6,7 @@ D=$(dirname $(realpath $0))
 echo "-- Day 3 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
 $D/../../lang/node.sh				"$D/solutions/node/stelar7/main.mjs"	"$D/io/*"
+$D/../../lang/python3.sh			"$D/solutions/python/kurainu/main.py"   "$D/io/*"
 
 
 echo ""
