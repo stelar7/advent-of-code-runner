780bbcc26e6c789aa32cc856dc593fbcefca2557
5late <christianrfernandes5@gmail.com>
2021-12-02 20:23:45 -0500

    added part 2 day 1

--- 2021/1/solutions/perl/slate/day1.pl
+++ 2021/1/solutions/perl/slate/day1.pl
@@ -4,12 +4,23 @@ $past = 0;
 $current = 0;
 $counter = 0;
 
+$threepast = 0;
+$twopast = 0;
+$countertwo = -2;
+
 while (<>) {
     if ($current > $past) {
         $counter += 1;
     }
+    if (($threepast + $twopast + $past) < ($twopast + $past + $current)) {
+	    $countertwo += 1;
+    }
+
+    $threepast = $twopast;
+    $twopast = $past;
     $past = $current;
     $current = $_;
 }
 
-print $counter;
+print $counter, "\n";
+print $countertwo, "\n";
