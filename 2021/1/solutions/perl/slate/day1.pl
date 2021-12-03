5d06a67d8d39c4b9a1331fd06b2710b73008a7d6
5late <christianrfernandes5@gmail.com>
2021-12-02 21:14:34 -0500

    fixed perl code FINALLY

--- 2021/1/solutions/perl/slate/day1.pl
+++ 2021/1/solutions/perl/slate/day1.pl
@@ -1,30 +1,20 @@
 #!/usr/bin/perl
-
-$past = 0;
-$current = 0;
-$counter = 0;
-
-$threepast = 0;
-$twopast = 0;
-$countertwo = 0;
-
 my @arr = ();
 
 while (<>) {
     push @arr , $_;
-    if ($current > $past) {
-        $counter += 1;
-    }
-    $past = $current;
-    $current = $_;
 }
 
-$length = @arr
+my $length;
 
-for ($i=0; $i<@arr; ++$i) {
-    if ((@arr[i+1] + @arr[i+2] + @arr[i+3]) < (@arr[i+2] + @arr[i+1] + @arr[i])) {
+foreach (@arr) {
+    if (@arr[$length+1] > @arr[$length]) {
+        $counter += 1;
+    }
+    if ((@arr[$length+1] + @arr[$length+2] + @arr[$length+3]) > (@arr[$length] + @arr[$length+1] + @arr[$length+2])) {
         $countertwo += 1;
     }
+    $length++;
 }
 
 print $counter, "\n";
