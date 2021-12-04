6be0936682f5b020450d44341eaa4350779c8e2a
Thijs Molendijk <info@thijsmolendijk.nl>
2021-12-04 13:57:30 +0100

    Day 4 - molenzwiebel

--- 2021/4/test.sh
+++ 2021/4/test.sh
@@ -7,6 +7,7 @@ echo "-- Day 4 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
 $D/../../lang/cmake_cpp.sh			"$D/solutions/cmake_cpp/querijn/"		    "$D/io/*"
 $D/../../lang/node.sh				"$D/solutions/node/stelar7/main.mjs"	    "$D/io/*"
+$D/../../lang/cargo_rust.sh			"$D/solutions/rust/molenzwiebel"	"$D/io/*"
 
 
 echo ""
