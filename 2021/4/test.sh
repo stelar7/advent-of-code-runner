dcf233bda575179bc45e408b53df81676c0599f8
Fernando Martín Besteiro <fmbesteiro@gmail.com>
2021-12-05 18:54:48 -0300

    Add test

--- 2021/4/test.sh
+++ 2021/4/test.sh
@@ -10,6 +10,7 @@ $D/../../lang/node.sh				"$D/solutions/node/stelar7/main.mjs"	    "$D/io/*"
 $D/../../lang/cargo_rust.sh			"$D/solutions/rust/molenzwiebel"	        "$D/io/*"
 $D/../../lang/python3.sh			"$D/solutions/python/kurainu/main.py"	        "$D/io/*"
 $D/../../lang/cargo_rust.sh			"$D/solutions/rust/mingweisamuel"	"$D/io/*"
+$D/../../lang/dotnet_c_sharp.sh		"$D/solutions/dotnet_c_sharp/renkon"		"$D/io/*"
 
 
 echo ""
