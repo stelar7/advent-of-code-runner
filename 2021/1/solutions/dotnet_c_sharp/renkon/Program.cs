03e7f679ab5c21751c7011fcfc268adcaa210a1b
Fernando Martín Besteiro <fmbesteiro@gmail.com>
2021-12-05 18:05:12 -0300

    Redone problem 1

--- 2021/1/solutions/dotnet_c_sharp/renkon/Program.cs
+++ 2021/1/solutions/dotnet_c_sharp/renkon/Program.cs
@@ -1,35 +1,25 @@
-﻿List<int> n = new List<int>(2000);
-int c1 = 0, c2 = 0;
-int i = 0;
-string s;
-while ((s = Console.ReadLine()) != null)
+﻿using renkon;
+
+int DoPartA()
 {
-    n.Add(0);
-
-    for (int j = 0; j < s.Length; j++)
-    {
-        n[i] = n[i] * 10 + (s[j] - '0');
-    }
-
-    if (i >= 3)
-    {
-        if (n[i - 3] < n[i])
-        {
-            c2++;
-        }
-
-        if (n[i - 1] < n[i])
-        {
-            c1++;
-        }
-    }
-    else if (i >= 1 && n[i - 1] < n[i])
-    {
-        c1++;
-    }
-
-    i++;
+    return CountIncreasesByStep(GetListOfIntegers(), 1);
 }
 
-Console.WriteLine(c1);
-Console.WriteLine(c2);
+int DoPartB()
+{
+    return CountIncreasesByStep(GetListOfIntegers(), 3);
+}
+
+int CountIncreasesByStep(IList<int> numbers, int stepSize)
+{
+    return numbers.Take(new Range(0, numbers.Count - stepSize))
+        .Select((n, i) => n - numbers[i + stepSize])
+        .Count(n => n < 0);
+}
+
+IList<int> GetListOfIntegers()
+{
+    return Utils.StringArrayToIntArray(Utils.InputToStringArray("1")).ToList();
+}
+
+Console.WriteLine(DoPartA() + "\n" + DoPartB());
