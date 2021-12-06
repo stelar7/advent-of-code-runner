dfa253b8ba5d5e70d7b19e9d64addfd82d6cff1d
O. Kunc <48525283+AoshiW@users.noreply.github.com>
2021-12-06 09:31:16 +0100

    add test.sh

--- 2021/6/test.sh
+++ 2021/6/test.sh
@@ -7,6 +7,7 @@ echo "-- Day 6 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
 $D/../../lang/cmake_cpp.sh			"$D/solutions/cmake_cpp/querijn/"	    	"$D/io/*"
 $D/../../lang/node.sh			    "$D/solutions/node/stelar7/main.mjs"		"$D/io/*"
+$D/../../lang/dotnet_c_sharp.sh		    "$D/solutions/dotnet_c_sharp/aoshiw"	    "$D/io/*"
 
 
 echo ""
