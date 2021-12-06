40aa11d7eb8d9bf91ab2316a71368035b6775fed
stelar7 <dudedbz@gmail.com>
2021-12-06 21:31:23 +0100

    Adjust alignment

--- 2021/0/test.sh
+++ 2021/0/test.sh
@@ -5,9 +5,9 @@ D=$(dirname $(realpath $0))
 
 echo "-- Day 0 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
-$D/../../lang/node.sh   "$D/solutions/node/example/main.mjs"  "$D/io/*"
-$D/../../lang/node.sh   "$D/solutions/node/slow/main.mjs"  "$D/io/*"
-$D/../../lang/node.sh   "$D/solutions/node/wrong/main.mjs"  "$D/io/*"
-$D/../../lang/node.sh   "$D/solutions/node/too-slow/main.mjs"  "$D/io/*"
+$D/../../lang/node.sh       "$D/solutions/node/example/main.mjs"        "$D/io/*"
+$D/../../lang/node.sh       "$D/solutions/node/slow/main.mjs"           "$D/io/*"
+$D/../../lang/node.sh       "$D/solutions/node/wrong/main.mjs"          "$D/io/*"
+$D/../../lang/node.sh       "$D/solutions/node/too-slow/main.mjs"       "$D/io/*"
 
 echo ""
--- 2021/5/test.sh
+++ 2021/5/test.sh
@@ -5,10 +5,10 @@ D=$(dirname $(realpath $0))
 
 echo "-- Day 5 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
-$D/../../lang/cmake_cpp.sh			"$D/solutions/cmake_cpp/querijn/"	    	"$D/io/*"
-$D/../../lang/node.sh			    "$D/solutions/node/stelar7/main.mjs"		"$D/io/*"
-$D/../../lang/cargo_rust.sh			"$D/solutions/rust/molenzwiebel"	        "$D/io/*"
-$D/../../lang/dotnet_c_sharp.sh		"$D/solutions/dotnet_c_sharp/renkon"		"$D/io/*"
-$D/../../lang/cargo_rust.sh			"$D/solutions/rust/mingweisamuel"			"$D/io/*"
+$D/../../lang/cmake_cpp.sh              "$D/solutions/cmake_cpp/querijn/"           "$D/io/*"
+$D/../../lang/node.sh                   "$D/solutions/node/stelar7/main.mjs"        "$D/io/*"
+$D/../../lang/cargo_rust.sh             "$D/solutions/rust/molenzwiebel"            "$D/io/*"
+$D/../../lang/dotnet_c_sharp.sh         "$D/solutions/dotnet_c_sharp/renkon"        "$D/io/*"
+$D/../../lang/cargo_rust.sh             "$D/solutions/rust/mingweisamuel"           "$D/io/*"
 
 echo ""
