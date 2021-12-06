817a92218c57e3845973757788bd8361c7dd989e
5late <christianrfernandes5@gmail.com>
2021-12-06 12:42:06 -0500

    added day 6 perl slate

--- 2021/6/test.sh
+++ 2021/6/test.sh
@@ -10,5 +10,6 @@ $D/../../lang/node.sh			    "$D/solutions/node/stelar7/main.mjs"		"$D/io/*"
 $D/../../lang/dotnet_c_sharp.sh		    "$D/solutions/dotnet_c_sharp/aoshiw"	    "$D/io/*"
 $D/../../lang/cargo_rust.sh			"$D/solutions/rust/molenzwiebel"	        "$D/io/*"
 $D/../../lang/dotnet_c_sharp.sh			"$D/solutions/dotnet_c_sharp/renkon"		"$D/io/*"
+$D/../../lang/perl.sh				"$D/solutions/perl/slate/day6.pl"		"$D/io/*"
 
 echo ""
