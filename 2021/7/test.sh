548c22502d388f914e7a68bd5fc01a3cacdd6f22
Querijn Heijmans <git@irule.at>
2021-12-07 22:10:42 +0100

    Update test.sh

--- 2021/7/test.sh
+++ 2021/7/test.sh
@@ -5,6 +5,7 @@ D=$(dirname $(realpath $0))
 
 echo "-- Day 7 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
+$D/../../lang/cmake_cpp.sh         "$D/solutions/cmake_cpp/querijn/"           "$D/io/*"
 $D/../../lang/python3.sh        "$D/solutions/python/slate/d7.py"           "$D/io/*"
 $D/../../lang/perl.sh		"$D/solutions/perl/slate/day7.pl"		    "$D/io/*"
 $D/../../lang/node.sh           "$D/solutions/node/stelar7/main.mjs"        "$D/io/*"
