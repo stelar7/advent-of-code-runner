640fc7e90c4b48aa40fd8be8427d7ec05087807d
5late <christianrfernandes5@gmail.com>
2021-12-07 00:54:19 -0500

    day 7 slate python

--- 2021/7/test.sh
+++ 2021/7/test.sh
@@ -5,7 +5,7 @@ D=$(dirname $(realpath $0))
 
 echo "-- Day 7 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
-
+$D/../../lang/python3.sh		"$D/solutions/python/slate/d7.py"		"$D/io/*"
 
 
 echo ""
