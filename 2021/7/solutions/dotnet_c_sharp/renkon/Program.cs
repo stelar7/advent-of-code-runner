b1825db5894f81800bc6b0e0244611583e2a0920
Fernando Martín Besteiro <fmbesteiro@gmail.com>
2021-12-07 11:15:05 -0300

    Problem 7

---
+++ 2021/7/solutions/dotnet_c_sharp/renkon/Program.cs
@@ -0,0 +1,25 @@
+﻿using renkon;
+
+long DoPartA()
+{
+    var numbers = Utils.InputToStringArray("7").First().Split(",").Select(int.Parse);
+    return GetOptimalDistance(numbers, (n1, n2) => Math.Abs(n1 - n2), n => n);
+}
+
+long DoPartB()
+{
+    var numbers = Utils.InputToStringArray("7").First().Split(",").Select(int.Parse);
+    return GetOptimalDistance(numbers, (n1, n2) => Math.Abs(n1 - n2), n => (n * (n + 1)) / 2);
+}
+
+long GetOptimalDistance(
+    IEnumerable<int> numbers,
+    Func<long, long, long> fnGen,
+    Func<long, long> fnSum)
+{
+    return Utils.GetRangeBetween(numbers.Min(), numbers.Max())
+        .Select(n => numbers.Select(n2 => fnSum.Invoke(fnGen.Invoke(n2, n))).Sum())
+        .Min();
+}
+
+Console.WriteLine(DoPartA() + "\n" + DoPartB());
--- 2021/7/test.sh
+++ 2021/7/test.sh
@@ -9,6 +9,6 @@ $D/../../lang/python3.sh        "$D/solutions/python/slate/d7.py"           "$D/
 $D/../../lang/node.sh           "$D/solutions/node/stelar7/main.mjs"        "$D/io/*"
 $D/../../lang/cargo_rust.sh     "$D/solutions/rust/mingweisamuel"           "$D/io/*"
 $D/../../lang/dotnet_c_sharp.sh     "$D/solutions/dotnet_c_sharp/aoshiw"        "$D/io/*"
-
+$D/../../lang/dotnet_c_sharp.sh	"$D/solutions/dotnet_c_sharp/renkon"		"$D/io/*"
 
 echo ""
