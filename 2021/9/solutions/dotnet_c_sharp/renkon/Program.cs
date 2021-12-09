34f0211b45869a8448f3d2bb8bcdd40bf5bfaadd
Fernando Martín Besteiro <fmbesteiro@gmail.com>
2021-12-09 11:02:04 -0300

    Fix 9

--- 2021/9/solutions/dotnet_c_sharp/renkon/Program.cs
+++ 2021/9/solutions/dotnet_c_sharp/renkon/Program.cs
@@ -2,15 +2,18 @@
 
 int DoPartA()
 {
-    var heightMap = Utils.InputToStringArray("9").Select(s => s.ToCharArray().Select(c => c - '0').ToArray()).ToArray();
+    var heightMap = Utils.InputToStringArray("9")
+        .Where(s => !string.IsNullOrEmpty(s))
+        .Select(s => s.ToCharArray().Select(c => c - '0').ToArray())
+        .ToArray();
     var riskLevelSum = 0;
     for (int i = 0; i < heightMap[0].Length; i++)
     {
         for (int j = 0; j < heightMap.Length; j++)
         {
-            if (IsLowerPoint(heightMap, i, j))
+            if (IsLowerPoint(heightMap, j, i))
             {
-                riskLevelSum += heightMap[i][j] + 1;
+                riskLevelSum += heightMap[j][i] + 1;
             }
         }
     }
@@ -20,7 +23,10 @@ int DoPartA()
 
 int DoPartB()
 {
-    var heightMap = Utils.InputToStringArray("9").Select(s => s.ToCharArray().Select(c => c - '0').ToArray()).ToArray();
+    var heightMap = Utils.InputToStringArray("9")
+        .Where(s => !string.IsNullOrEmpty(s))
+        .Select(s => s.ToCharArray().Select(c => c - '0').ToArray())
+        .ToArray();
     var visitMap = heightMap.Select(sm => new bool[sm.Length]).ToArray();
     SetHeightAsVisited(9, heightMap, visitMap);
 
@@ -29,7 +35,7 @@ int DoPartB()
     {
         for (int j = 0; j < heightMap.Length; j++)
         {
-            if (!visitMap[i][j])
+            if (!visitMap[j][i])
             {
                 basinLengths.Add(GetBasinLengthFromPoint(heightMap, visitMap, i, j));
             }
@@ -45,37 +51,37 @@ void SetHeightAsVisited(int height, int[][] heightMap, bool[][] visitMap)
     {
         for (int j = 0; j < heightMap.Length; j++)
         {
-            if (heightMap[i][j] == height)
+            if (heightMap[j][i] == height)
             {
-                visitMap[i][j] = true;
+                visitMap[j][i] = true;
             }
         }
     }
 }
 
-int GetBasinLengthFromPoint(int[][] map, bool[][] visits, int x, int y)
+int GetBasinLengthFromPoint(int[][] map, bool[][] visits, int y, int x)
 {
     var lengthCount = 1;
     visits[x][y] = true;
 
     if (x > 0 && !visits[x - 1][y])
     {
-        lengthCount += GetBasinLengthFromPoint(map, visits, x - 1, y);
+        lengthCount += GetBasinLengthFromPoint(map, visits, y, x - 1);
     }
 
-    if (x < map[0].Length - 1 && !visits[x + 1][y])
+    if (x < map.Length - 1 && !visits[x + 1][y])
     {
-        lengthCount += GetBasinLengthFromPoint(map, visits, x + 1, y);
+        lengthCount += GetBasinLengthFromPoint(map, visits, y, x + 1);
     }
 
     if (y > 0 && !visits[x][y - 1])
     {
-        lengthCount += GetBasinLengthFromPoint(map, visits, x, y - 1);
+        lengthCount += GetBasinLengthFromPoint(map, visits, y - 1, x);
     }
 
-    if (y < map.Length - 1 && !visits[x][y + 1])
+    if (y < map[0].Length - 1 && !visits[x][y + 1])
     {
-        lengthCount += GetBasinLengthFromPoint(map, visits, x, y + 1);
+        lengthCount += GetBasinLengthFromPoint(map, visits, y + 1, x);
     }
 
     return lengthCount;
@@ -83,8 +89,8 @@ int GetBasinLengthFromPoint(int[][] map, bool[][] visits, int x, int y)
 
 bool IsLowerPoint(int[][] map, int x, int y)
     => !((x > 0 && map[x - 1][y] <= map[x][y]) ||
-        (x < map[0].Length - 1 && map[x + 1][y] <= map[x][y]) ||
+        (x < map.Length - 1 && map[x + 1][y] <= map[x][y]) ||
         (y > 0 && map[x][y - 1] <= map[x][y]) ||
-        (y < map.Length - 1 && map[x][y + 1] <= map[x][y]));
+        (y < map[0].Length - 1 && map[x][y + 1] <= map[x][y]));
 
 Console.WriteLine(DoPartA() + "\n" + DoPartB());
