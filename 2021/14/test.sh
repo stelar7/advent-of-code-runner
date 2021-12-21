1e0a67ac6f4eebf653125b9f42b4173055d240d1
stelar7 <dudedbz@gmail.com>
2021-12-21 23:14:50 +0100

    Update test.sh

--- 2021/14/test.sh
+++ 2021/14/test.sh
@@ -7,6 +7,7 @@ echo "-- Day 14 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
 $D/../../lang/cargo_rust.sh         "$D/solutions/rust/mingweisamuel"           "$D/io/*"
 $D/../../lang/cargo_rust.sh         "$D/solutions/rust/mingweisamuel2"          "$D/io/*"
+$D/../../lang/node.sh               "$D/solutions/node/stelar7/main.mjs"        "$D/io/*"
 
 
 echo ""
