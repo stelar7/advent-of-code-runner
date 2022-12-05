ded3c27016bbfd5c692d0e5a7c7c813a2c943dc6
AoshiW <48525283+AoshiW@users.noreply.github.com>
2022-12-05 19:05:51 +0100

    fix dotned

--- lang/dotnet_c_sharp_build.sh
+++ lang/dotnet_c_sharp_build.sh
@@ -9,6 +9,6 @@ rm -rf "$SOLUTION/bin"
 
 START=$($D/util/start.sh)
 
-timeout --signal=SIGKILL 120s publish -c Release -r linux-x64 -o bin/output "$SOLUTION" >/dev/null
+timeout --signal=SIGKILL 120s dotnet publish -c Release -r linux-x64 -o bin/output "$SOLUTION" >/dev/null
 
 echo $($D/util/stop.sh $START)
