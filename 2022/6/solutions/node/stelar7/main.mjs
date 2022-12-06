f7a8a7ee28a67cb49dea25cbc2985510e74704e9
stelar7 <dudedbz@gmail.com>
2022-12-06 08:43:19 +0100

    Fix formatting

--- 2022/6/solutions/node/stelar7/main.mjs
+++ 2022/6/solutions/node/stelar7/main.mjs
@@ -8,7 +8,7 @@ const lines = readFileSync(STANDARD_IN).toString().split("\n").slice(0, -1);
 const line = lines[0];
 for (let i = 0; i < line.length - 4; i++) {
     let unique = new Set();
-    for (let j = i; j < i+4; j++) {
+    for (let j = i; j < i + 4; j++) {
         unique.add(line[j]);
     }
     if (unique.size == 4) {
@@ -19,7 +19,7 @@ for (let i = 0; i < line.length - 4; i++) {
 
 for (let i = 0; i < line.length - 4; i++) {
     let unique2 = new Set();
-    for (let j = i; j < i+14; j++) {
+    for (let j = i; j < i + 14; j++) {
         unique2.add(line[j]);
     }
     if (unique2.size == 14) {
