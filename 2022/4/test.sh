9164141107e33329a6a3751eac49e13ea6adc955
5late <christianrfernandes5@gmail.com>
2022-12-09 14:19:38 -0500

    added all test files

--- 2022/4/test.sh
+++ 2022/4/test.sh
@@ -7,5 +7,6 @@ echo "-- Day 4 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
 $D/../../lang/node.sh                   "$D/solutions/node/stelar7/main.mjs"            "$D/io/*"
 $D/../../lang/golfscript.sh             "$D/solutions/golfscript/pseudonym117/day4.gs"  "$D/io/*"
+$D/../../lang/python3.sh		"$D/solutions/python/slate/d4.py"		"$D/io/*"
 
 echo ""
--- 2022/6/test.sh
+++ 2022/6/test.sh
@@ -7,5 +7,6 @@ echo "-- Day 6 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
 $D/../../lang/node.sh                   "$D/solutions/node/stelar7/main.mjs"            "$D/io/*"
 $D/../../lang/golfscript.sh             "$D/solutions/golfscript/pseudonym117/day6.gs"  "$D/io/*"
+$D/../../lang/python3.sh		"$D/solutions/python/slate/d6.py"		"$D/io/*"
 
 echo ""
