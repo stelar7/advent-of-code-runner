cbe0d8402b3568deac5a52a157b1f11395769f9e
5late <christianrfernandes5@gmail.com>
2022-12-09 14:13:46 -0500

    fix day 1

--- 2022/1/solutions/python/slate/d1.py
+++ 2022/1/solutions/python/slate/d1.py
@@ -12,16 +12,18 @@ for line in input:
 			sum_cals += int(cal)
 		total_cals.append(sum_cals)
 		cals.clear()
+	elif line == input[-1]:
+		total_cals.append(int(line.rstrip()))
 	else:
 		cals.append(line.rstrip())
+
+print(max(total_cals))
 
 top_three = []
 
 for i in range(3):
 	top_three.append(max(total_cals))
 	total_cals.remove(max(total_cals))
-
-print(max(total_cals))
 
 total = sum(top_three)
 
