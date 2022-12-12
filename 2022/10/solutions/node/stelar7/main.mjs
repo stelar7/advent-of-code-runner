021ad6943b8ffbdca42f751ca8083e83be42ca7a
stelar7 <dudedbz@gmail.com>
2022-12-12 11:03:13 +0100

    Day 10

---
+++ 2022/10/solutions/node/stelar7/main.mjs
@@ -0,0 +1,58 @@
+import {
+    readFileSync
+} from "fs";
+import {
+    exit
+} from "process";
+
+const STANDARD_IN = 0;
+const lines = readFileSync(STANDARD_IN).toString().split("\n").slice(0, -1);
+
+let cycle = 1;
+let counter = 1;
+
+let output = 0;
+let outputString = "";
+
+function sumSignal() {
+    if ((cycle + 20) % 40 == 0) {
+        output += cycle * counter;
+    }
+}
+
+function drawImage() {
+    let center = ((cycle - 1) % 40);
+    if (counter - 1 == center || counter == center || counter + 1 == center) {
+        outputString += "#";
+    } else {
+        outputString += ".";
+    }
+
+    if (cycle % 40 == 0) {
+        outputString += "\n";
+    }
+}
+
+for (const element of lines) {
+    const line = element;
+
+    if (line.startsWith("noop")) {
+        drawImage();
+        cycle++;
+        sumSignal();
+        continue;
+    }
+
+    drawImage();
+    cycle++;
+    sumSignal();
+
+    drawImage();
+    cycle++;
+    const count = +line.substring(4, line.length);
+    counter += count;
+    sumSignal();
+}
+
+console.log(output);
+console.log(outputString);
