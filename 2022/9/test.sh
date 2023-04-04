19c45c6f463875487eb3c6d2240425484dc7efed
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-04-03 20:07:41 -0700

    mingweisamuel day 9

--- 2022/9/test.sh
+++ 2022/9/test.sh
@@ -6,5 +6,6 @@ D=$(dirname $(realpath $0))
 echo "-- Day 9 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
 $D/../../lang/node.sh                   "$D/solutions/node/stelar7/main.mjs"            "$D/io/*"
+$D/../../lang/cargo_rust.sh             "$D/solutions/rust/mingweisamuel"               "$D/io/*"
 
 echo ""
