aeb5ea54ba597ef61d5c23e22fea1f63318ccecd
stelar7 <dudedbz@gmail.com>
2023-12-01 09:21:14 +0100

    add script to create days

---
+++ createdays.sh
@@ -0,0 +1,10 @@
+for X in {1..25..1} 
+do 
+mkdir "2023/$X" 
+mkdir "2023/$X/io" 
+mkdir "2023/$X/solutions" 
+cp lang/util/base_test_script.sh "2023/$X/test.sh"
+sed -i "s/Day 1/Day $X/g" "2023/$X/test.sh"
+chmod +x "2023/$X/test.sh"
+
+done
