acf39febaf1f9d67d90d3462e0d4105623e9099f
stelar7 <dudedbz@gmail.com>
2023-12-01 09:29:37 +0100

    Create empty readmes

--- createdays.sh
+++ createdays.sh
@@ -3,6 +3,7 @@ do
 mkdir "2023/$X" 
 mkdir "2023/$X/io" 
 mkdir "2023/$X/solutions" 
+touch "2023/$X/README.md"
 cp lang/util/base_test_script.sh "2023/$X/test.sh"
 sed -i "s/Day 1/Day $X/g" "2023/$X/test.sh"
 chmod +x "2023/$X/test.sh"
