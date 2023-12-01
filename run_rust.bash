278eddbd399c26d0035af7f02ab47be50b7ef6c0
stelar7 <dudedbz@gmail.com>
2023-12-01 09:14:02 +0100

    remove run script that shouldnt exist

--- run_rust.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Usage: `$0 <USER> <DAY> [INPUT]`
-# Example: `$0 mingweisamuel 5 example`
-set -eux
-
-INPUT=${3:-$1}
-cat "2022/$2/io/$INPUT.input" | cargo run --manifest-path "2022/$2/solutions/rust/$1/Cargo.toml"
