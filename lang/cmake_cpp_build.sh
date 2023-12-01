14e129d03e22ded45f86a80f755a60402f305298
stelar7 <dudedbz@gmail.com>
2023-12-01 10:09:30 +0100

    output cpp logs

--- lang/cmake_cpp_build.sh
+++ lang/cmake_cpp_build.sh
@@ -3,6 +3,8 @@ set -uo pipefail
 D=$(dirname $(realpath $0))
 
 SOLUTION=$1
+LOGPATH="$D/../logs/$(basename $(dirname $SOLUTION))/${SOLUTION##*/}"
+mkdir -p $LOGPATH
 
 rm -rf $SOLUTION/build
 mkdir $SOLUTION/build
@@ -10,7 +12,7 @@ cd $SOLUTION/build
 
 START=$($D/util/start.sh)
 
-timeout --signal=SIGKILL 120s cmake -G Ninja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_BUILD_TYPE=Release .. >/dev/null
-timeout --signal=SIGKILL 120s ninja >/dev/null
+timeout --signal=SIGKILL 120s cmake -G Ninja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_BUILD_TYPE=Release .. > $LOGPATH/cmake.log 2>&1
+timeout --signal=SIGKILL 120s ninja > $LOGPATH/ninja.log 2>&1
 
 echo $($D/util/stop.sh $START)
