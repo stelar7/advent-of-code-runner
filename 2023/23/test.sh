fd8a514fa5e6f62d5a263968fbc167280eedc466
stelar7 <dudedbz@gmail.com>
2023-12-01 09:21:31 +0100

    add runner for remaining days

---
+++ 2023/23/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+D=$(dirname $(realpath $0))
+
+echo "-- Day 23 --"
+printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
+
+
+echo ""
