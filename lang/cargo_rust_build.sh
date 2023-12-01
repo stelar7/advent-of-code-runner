48b8851d423cee3a0a560f748cc4c97da0c78b59
stelar7 <dudedbz@gmail.com>
2023-12-01 15:00:06 +0100

    Add log output to rust too

--- lang/cargo_rust_build.sh
+++ lang/cargo_rust_build.sh
@@ -3,13 +3,15 @@ set -uo pipefail
 D=$(dirname $(realpath $0))
 
 SOLUTION=$1
+LOGPATH="$D/../logs/$(basename $(dirname $SOLUTION))/${SOLUTION##*/}"
+mkdir -p $LOGPATH
 
 rm -rf $SOLUTION/target
 
-cargo install lazy_static > /dev/null 2>&1
+cargo install lazy_static > $LOGPATH/cargo_install.log 2>&1
 
 START=$($D/util/start.sh)
 
-timeout --signal=SIGKILL 120s cargo build --manifest-path "$SOLUTION/Cargo.toml" --release > /dev/null 2>&1
+timeout --signal=SIGKILL 120s cargo build --manifest-path "$SOLUTION/Cargo.toml" --release > $LOGPATH/cargo_build.log 2>&1
 
 echo $($D/util/stop.sh $START)
