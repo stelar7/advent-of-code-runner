f5cc39d023aa7644425bd479a6b3a8e25dad7836
TheDrone7 <h@thedrone7.dev>
2023-12-02 14:10:41 -0500

    Add solution to tests

--- 2023/2/test.sh
+++ 2023/2/test.sh
@@ -8,5 +8,6 @@ printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "t
 $D/../../lang/cmake_cpp.sh               "$D/solutions/cpp/querijn"                        "$D/io/*"
 $D/../../lang/elixir.sh                  "$D/solutions/elixir/leastrio/main.exs"           "$D/io/*"
 $D/../../lang/node.sh                    "$D/solutions/node/stelar7/main.mjs"              "$D/io/*"
+$D/../../lang/cargo_rust.sh              "$D/solutions/rust/thedrone7"		                 "$D/io/*"
 
 echo ""
