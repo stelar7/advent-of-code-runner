efb967f23605a0dc6778c54a5ce754e4f26b4837
pseudonym117 <godtheresnonamesleft@gmail.com>
2023-12-03 13:47:42 -0700

    Add reusable main method for less copy paste 🗑️

--- 2023/1/solutions/cpp/pseudonym117/main.cpp
+++ 2023/1/solutions/cpp/pseudonym117/main.cpp
@@ -1,30 +1,11 @@
-#include <chrono>
+#include <utils.hpp>
 
 #include "day1.hpp"
-#include "util.hpp"
 
 using namespace pseudonym117;
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        return -1;
-    }
-    auto filename = argv[1];
-
-    bool bench = argc > 2 && argv[2] == std::string("--bench");
-
-    if (bench) {
-        const int iterations = 10000;
-        auto start = std::chrono::high_resolution_clock::now();
-        for (auto i = 0; i < iterations; ++i) {
-            day1<noop_reporter>(filename);
-        }
-        auto end = std::chrono::high_resolution_clock::now();
-
-        auto duration = end - start;
-        auto runtime = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
-        std::cout << "total time: " << runtime << ", average: " << runtime / (double)iterations << std::endl;
-    } else {
-        day1<stdout_reporter>(filename);
-    }
+int main(int argc, const char* argv[]) {
+    main_runner<day1> runner;
+    
+    return runner(argc, argv);
 }
