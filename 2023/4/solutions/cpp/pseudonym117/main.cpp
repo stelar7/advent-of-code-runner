413157c067bd4552c538c744b1ab45f80a1f8d6d
AG Stephan <godtheresnonamesleft@gmail.com>
2023-12-05 16:48:47 -0700

    pseudonym117 day 4

---
+++ 2023/4/solutions/cpp/pseudonym117/main.cpp
@@ -0,0 +1,9 @@
+#include <utils.hpp>
+#include "day4.hpp"
+
+using namespace pseudonym117;
+
+int main(int argc, const char* argv[]) {
+    main_runner<day4> runner;
+    return runner(argc, argv);
+}
