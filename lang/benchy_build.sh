99780768ad83543c61b8729a497a69aa9d40248d
Querijn Heijmans <fork@irule.at>
2023-12-06 16:58:54 +0100

    No more fancy logs

--- lang/benchy_build.sh
+++ lang/benchy_build.sh
@@ -2,10 +2,7 @@
 set -uo pipefail
 D=$(dirname $(realpath $0))
 
-LOGPATH="$D/../logs/benchy/"
-mkdir -p $LOGPATH
-
 cd $D/util/bench
 
-timeout --signal=SIGKILL 120s cmake -G Ninja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_BUILD_TYPE=Release . > $LOGPATH/cmake.log 2>&1
-timeout --signal=SIGKILL 120s ninja > $LOGPATH/ninja.log 2>&1
+timeout --signal=SIGKILL 120s cmake -G Ninja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_BUILD_TYPE=Release .
+timeout --signal=SIGKILL 120s ninja
