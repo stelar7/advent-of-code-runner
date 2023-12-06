45680d212fabc76ae9e45003bc9c92c94f757b41
Querijn Heijmans <fork@irule.at>
2023-12-06 17:45:17 +0100

    Remove test output

--- 2023/3/test.sh
+++ 2023/3/test.sh
@@ -2,9 +2,6 @@
 set -euo pipefail
 
 D=$(dirname $(realpath $0))
-chmod +x $D/../../lang/benchy_build.sh
-chmod +x $D/../../lang/benchy_cpp.sh
-$D/../../lang/benchy_build.sh
 
 echo "-- Day 3 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
