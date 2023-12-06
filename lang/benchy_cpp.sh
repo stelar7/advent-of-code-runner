bad3667a8306c2ae06ef57331fd586c018184628
Querijn Heijmans <fork@irule.at>
2023-12-06 16:42:24 +0100

    Add initial version of benchy

---
+++ lang/benchy_cpp.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -uo pipefail
+D=$(dirname $(realpath $0))
+
+SOLUTION=$1
+LOGPATH="$D/../logs/$(basename $(dirname $SOLUTION))/${SOLUTION##*/}"
+mkdir -p $LOGPATH
+
+rm -rf $SOLUTION/build
+mkdir $SOLUTION/build
+cd $SOLUTION/build
+
+timeout --signal=SIGKILL 120s cmake -G Ninja -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_BUILD_TYPE=Release .. > $LOGPATH/cmake.log 2>&1
+timeout --signal=SIGKILL 120s ninja > $LOGPATH/ninja.log 2>&1
