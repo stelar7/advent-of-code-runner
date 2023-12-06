1890f7f44763b5343f3c3b6eb5648b640b0afd88
Querijn Heijmans <fork@irule.at>
2023-12-06 16:50:25 +0100

    Run benchy on test

--- 2023/3/test.sh
+++ 2023/3/test.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 D=$(dirname $(realpath $0))
+$D/../../lang/benchy_build.sh
 
 echo "-- Day 3 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
