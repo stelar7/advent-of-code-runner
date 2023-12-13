e5f7a66baf7865eb6f07638281e66173a3aaf3c9
Token07 <tokenhd7@gmail.com>
2023-12-12 22:38:35 -0500

    Token07 day 11

--- 2023/11/test.sh
+++ 2023/11/test.sh
@@ -5,6 +5,6 @@ D=$(dirname $(realpath $0))
 
 echo "-- Day 11 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
-
+$D/../../lang/node.sh                    "$D/solutions/node/token07/11.js"              "$D/io/*"
 
 echo ""
