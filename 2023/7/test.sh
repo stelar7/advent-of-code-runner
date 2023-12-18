ce6c3f1b9a97c8b249526d37be0aeef9a1e503f7
pseudonym117 <godtheresnonamesleft@gmail.com>
2023-12-18 16:24:54 -0600

    pseudonym day 7

--- 2023/7/test.sh
+++ 2023/7/test.sh
@@ -7,5 +7,6 @@ echo "-- Day 7 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
 $D/../../lang/cargo_rust.sh              "$D/solutions/rust/FX"                            "$D/io/*"
 $D/../../lang/node.sh                    "$D/solutions/node/stelar7/main.mjs"              "$D/io/*"
+$D/../../lang/cmake_cpp.sh               "$D/solutions/cpp/pseudonym117"                   "$D/io/*"
 
 echo ""
