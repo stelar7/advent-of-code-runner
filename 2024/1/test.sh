82d10f2caf6e4f95913ac52f032237c2180255a1
Renja <76645494+renja-g@users.noreply.github.com>
2024-12-02 20:33:23 +0100

    Update 2024/1/test.sh
    Co-authored-by: stelar7 <dudedbz@gmail.com>

--- 2024/1/test.sh
+++ 2024/1/test.sh
@@ -6,6 +6,6 @@ D=$(dirname $(realpath $0))
 echo "-- Day 1 --"
 printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
 $D/../../lang/node.sh                    "$D/solutions/node/stelar7/main.mjs"              "$D/io/*"
-$D/../../lang/python3.sh                 "$D/solutions/python/ayato/main.py"               "$D/io/*"
+$D/../../lang/python3.sh                 "$D/solutions/python/renja-g/main.py"               "$D/io/*"
 
 echo ""
