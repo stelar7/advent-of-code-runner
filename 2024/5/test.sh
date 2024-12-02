19eea2ce46ec2c406f13c9de8a6d86778a168282
stelar7 <dudedbz@gmail.com>
2024-12-02 09:50:25 +0100

    Create runner files for 2024

---
+++ 2024/5/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+D=$(dirname $(realpath $0))
+
+echo "-- Day 5 --"
+printf "%-10s %-15s %-10s %-10s %-10s %-10s \n" "language" "author" "compile" "total" "average" "status"
+
+
+echo ""
--- createdays.sh
+++ createdays.sh
@@ -1,11 +1,13 @@
-for X in {1..25..1} 
+YEAR=2024
+mkdir "$YEAR" 
+for X in $(seq 1 25);
 do 
-mkdir "2023/$X" 
-mkdir "2023/$X/io" 
-mkdir "2023/$X/solutions" 
-touch "2023/$X/README.md"
-cp lang/util/base_test_script.sh "2023/$X/test.sh"
-sed -i "s/Day 1/Day $X/g" "2023/$X/test.sh"
-chmod +x "2023/$X/test.sh"
+mkdir "$YEAR/$X" 
+mkdir "$YEAR/$X/io" 
+mkdir "$YEAR/$X/solutions" 
+touch "$YEAR/$X/README.md"
+cp lang/util/base_test_script.sh "$YEAR/$X/test.sh"
+sed -i "s/Day 1/Day $X/g" "$YEAR/$X/test.sh"
+chmod +x "$YEAR/$X/test.sh"
 
 done
